ab426f0fd3134a84ab68968fa12394dc55b0dcf9
atharva_dawande <atharva.dawande603@gmail.com>
2020-09-26 00:49:41 +0530

    implemented interstitial ads too

--- app/src/main/java/com/theonedayapps/tapnswitch/MainActivity.java
+++ app/src/main/java/com/theonedayapps/tapnswitch/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 //import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -16,9 +17,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.ads.nativetemplates.NativeTemplateStyle;
 import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.formats.UnifiedNativeAd;
 import com.google.android.gms.ads.initialization.InitializationStatus;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity  {
     public static int what;
     public static boolean tobeornottobe=false;
     private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +59,19 @@ public class MainActivity extends AppCompatActivity  {
                 .build();
 
         adLoader.loadAd(new AdRequest.Builder().build());
+////////////////interstitial
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/8691691433");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
 
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+        });
 /////////////////////////////////////////////////////////////////////////////////////
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
@@ -67,6 +83,13 @@ public class MainActivity extends AppCompatActivity  {
        startbutton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               /////interstitial ad
+               if (mInterstitialAd.isLoaded()) {
+                   mInterstitialAd.show();
+               } else {
+                   Log.d("TAG", "The interstitial wasn't loaded yet.");
+               }
+               //////
                tobeornottobe=false;
                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
                    startService(new Intent(MainActivity.this, FloatingViewService.class));
